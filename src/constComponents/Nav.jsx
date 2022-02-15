a985bba82a8e97433f3e8018942b6210f6eb88f8
adarsh27april <adarshkumar27april@gmail.com>
2022-02-15 14:36:22 +0530

    navbar color changed

--- src/constComponents/Nav.jsx
+++ src/constComponents/Nav.jsx
@@ -2,35 +2,37 @@ import React from 'react';
 
 const Nav = () => {
 	return (<>
+		<header className="site-header sticky-top">
+			<nav className="mx-1 my-1 rounded navbar navbar navbar-expand-lg navbar-dark bg-secondary "
+				style={{ backgroundColor: "rgb(16, 17, 58)" }}
+			>
+				<div className="container-fluid">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="me-auto navbar-nav mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a target="blank" className="nav-link" href="https://github.com/adarsh27april/country-detail-react">
+									GitHub
+								</a>
+							</li>
+							<li className="nav-item">
+								<a href="/" className="nav-link">Home</a>
+							</li>
+						</ul>
 
-		<nav className="mx-1 my-1 rounded navbar navbar navbar-expand-lg navbar-dark bg-secondary ">
-			<div className="container-fluid">
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="me-auto navbar-nav mb-2 mb-lg-0">
-						<li className="nav-item">
-							<a target="blank" className="nav-link" href="https://github.com/adarsh27april/country-detail-react">
-								GitHub
-							</a>
-						</li>
-						<li className="nav-item">
-							<a href="/" className="nav-link">Home</a>
-						</li>
-					</ul>
+					</div>
 
+					<a href="/" className="navbar-brand">
+						Country Detail
+						<span className="container navbar-brand"></span>
+					</a>
 				</div>
 
-				<a href="/" className="navbar-brand">
-					Country Detail
-					<span className="container navbar-brand"></span>
-				</a>
-			</div>
-
-		</nav>
-
+			</nav>
+		</header>
 	</>);
 };
 
